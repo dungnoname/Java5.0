a3e1e728fcc38e71de3318cb4544d749d9d1523f
dungnoname <dung9012@gmail.com>
2025-10-16 12:12:43 +0700

    Initial commit

---
+++ src/main/java/com/poly/assigment/controller/HomeController.java
@@ -0,0 +1,53 @@
+package com.poly.assigment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/java5")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home/index";
+    }
+
+    @GetMapping("/SanPham/list")
+    public String gioiThieu() {
+        return "home/danhMucSanPham";
+    }
+
+    @GetMapping("/SanPham/detail/{id}")
+    public String chiTietSanPham(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("productId", id);
+        return "home/chiTietSanPham";
+    }
+
+    @GetMapping("/GioHang/list")
+    public String gioHang() {
+        return "home/gioHang";
+    }
+
+    @GetMapping("/TaiKhoan")
+    public String taiKhoan() {
+        return "home/taiKhoan";
+    }
+
+    @GetMapping("/donHang")
+    public String donHang() {
+        return "home/donHang";
+    }
+
+    @GetMapping("/dang-ky")
+    public String dangKy() {
+        return "home/dangKy";
+    }
+
+    @GetMapping("/dang-nhap")
+    public String dangNhap() {
+        return "home/dangNhap";
+    }
+}
